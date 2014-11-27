e12bc84d237e5c9c814f591d16ff781be7afae7a
damdamdamdan <daniel.ufir@students.ittralee.ie>
2014-11-27 22:30:41 +0000

    Blackjack game
    The start of the project

---
+++ Blackjack.java
@@ -0,0 +1,32 @@
+//Blackjack.java
+
+import java.awt.event.*;
+import java.awt.*;
+
+
+public class Blackjack extends JFrame implements ActionListener  
+{
+	public static void main(String args[])
+	{
+		Blackjack window = new Blackjack();
+		window.setVisible(true);
+		window.setSize(600,600);
+		
+			/*****************************************************
+*    Title:  
+*    Author: Hovercraft Full Of Eels
+*    Site owner/sponsor:  stackoverflow.com
+*    Date: 
+*    Code version:  
+*    Availability:  http://stackoverflow.com/questions/9543320/how-to-position-the-form-in-the-center-screen
+*    Modified:  
+*****************************************************/	
+		window.setLocationRelativeTo(null);
+		
+	}// end main
+	
+	
+} //end class
+
+
+
