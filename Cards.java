dfef97d24e9232e1179332ed65d458e6b5f3f499
damdamdamdan <daniel.ufir@students.ittralee.ie>
2014-12-03 12:16:55 +0000

    1

--- Cards.java
+++ Cards.java
@@ -3,55 +3,24 @@
 public class Cards extends BlackJackGame
 {	
 	//Start array for the cards
-	Random random = new Random();
-	private int [] cards = {2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,
-									10,10,10,10,10,10,10,10,11,11,11,11};	
 	
-	int playerScore;
-	int dealerScore;
+	LinkedList<String> linkedList = new LinkedList<String>();
 	
-	
-	public void playerS()
-	{		
-		if(playerScore == 21)
-		{
-			//Player wins
-			JOptionPane,showMessageDialog(null, "You win");
-			System.exit(0);
-		}
-		if(playerScore >21)
-		{
-			JOptionPane.showMessageDialog(null, "You lose");
-			System.exit(0);
-		}
-		if()
-				
-	}
-	
-	//call the players hand
-	public void playerHand()
-	{
-		int hand = random.nextInt(cards[0].length + cards[1].length);
-		if()//player hits hit add card
-		{
-			
-		}
+	linkedList.add("Two");
+	linkedList.add("Three");
+	linkedList.add("Four");
+	linkedList.add("Five");
+	linkedList.add("Six");
+	linkedList.add("Seven");
+	linkedList.add("Eight");
+	linkedList.add("Nine");
+	linkedList.add("Ten");
+	linkedList.add("Jack");
+	linkedList.add("Queen");
+	linkedList.add("King");
+	linkedList.add("Ace");
 		
-	}
-	
-	//get the dealers first 2 cards
-	public void dealerHand()
-	{
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
 }//end class
 
-  
+  //linkedList.remove() , linkedList.add(), linkedList.removeFirst(),
+  //linkedList.removeLast();, linkedList.set();
